ae816b8280eb5279b02adfd0fecdaf244563b12f
MagnusOstertag <MagnusOstertag@users.noreply.github.com>
2023-02-07 22:50:57 +0100

    MemSum data preparation

---
+++ config.py
@@ -0,0 +1,6 @@
+ENV_PATH = '/home/work/Dokumente/Studium/SimTech_MSc/Erasmus/Lectures/Text_mining/project/ExtractiveSummaryModels/' # to be changed
+DATA_PATH = 'data/'
+
+name_testset = 'test'
+name_trainset = 'train'
+name_valset = 'val'
