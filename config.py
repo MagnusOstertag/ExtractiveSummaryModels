9a2e5cd61bdace20dc789191b596c76ab3fe23b4
MagnusOstertag <MagnusOstertag@users.noreply.github.com>
2023-02-07 22:56:38 +0100

    Revert "MemSum data preparation"
    This reverts commit ae816b8280eb5279b02adfd0fecdaf244563b12f.

--- config.py
+++
@@ -1,6 +0,0 @@
-ENV_PATH = '/home/work/Dokumente/Studium/SimTech_MSc/Erasmus/Lectures/Text_mining/project/ExtractiveSummaryModels/' # to be changed
-DATA_PATH = 'data/'
-
-name_testset = 'test'
-name_trainset = 'train'
-name_valset = 'val'
